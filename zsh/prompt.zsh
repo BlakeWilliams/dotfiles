c855318fd88ae33662701556f4f6b4f5f8747cf9
Blake Williams <blake@blakewilliams.me>
2020-05-23 17:58:29 -0400

    Remove rprompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -7,14 +7,3 @@ _reset() { echo "%{$reset_color%}" }
 _bold() { echo "$lc${color[bold]}$rc" }
 
 PROMPT='$(_blue)%1~%{$reset_color%} %(?.$(_yellow).$(_red))λ%{$reset_color%} '
-
-autoload -Uz vcs_info
-precmd() { vcs_info }
-
-# # Format the vcs_info_msg_0_ variable
-zstyle ':vcs_info:git:*' formats '%b'
-
-# Set up the prompt (with git branch name)
-setopt PROMPT_SUBST
-setopt TRANSIENT_RPROMPT
-RPROMPT=' %B$(_green)${vcs_info_msg_0_}%{$reset_color%}'
