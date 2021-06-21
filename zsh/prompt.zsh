b272d05856c5db647130485f5f61317454f281ed
Blake Williams <blake@blakewilliams.me>
2021-06-20 23:16:37 -0400

    Update prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,9 +1,9 @@
 _red() { echo "%{$fg[red]%}" }
-_yellow() { echo "%{$fg[yellow]%}" }
+_magenta() { echo "%{$fg[magenta]%}" }
 _green() { echo "%{$fg[green]%}" }
 _blue() { echo "%{$fg[blue]%}" }
 _reset() { echo "%{$reset_color%}" }
 
 _bold() { echo "$lc${color[bold]}$rc" }
 
-PROMPT='$(_blue)%1~%{$reset_color%} %(?.$(_yellow).$(_red))λ%{$reset_color%} '
+PROMPT='$(_blue)%1~%{$reset_color%} %(?.$(_magenta).$(_red))❯%{$reset_color%} '
