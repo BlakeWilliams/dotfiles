c4630f5334633bd33bbb45195afe7f3c7b962440
Blake Williams <blake@blakewilliams.me>
2022-11-22 17:20:54 -0500

    Update neovim config for compat

--- config/nvim/experimental.vim
+++ config/nvim/experimental.vim
@@ -1,5 +1,6 @@
 lua <<EOF
 require'nvim-treesitter.configs'.setup {
+  ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   highlight = {
     enable = true,
   }
--- config/nvim/lua/lsp.lua
+++ config/nvim/lua/lsp.lua
@@ -10,71 +10,6 @@ vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(
   {border = 'rounded'}
 )
 
-local on_attach = function(client, bufnr)
-  local bufopts = { noremap=true, silent=true, buffer=bufnr }
-
-  local signs = { Error = "✕", Warn = "! ", Hint = "★ ", Info = "i " }
-  for type, icon in pairs(signs) do
-    local hl = "DiagnosticSign" .. type
-    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-  end
-
-  local format = function()
-    vim.lsp.buf.format {
-      async = true,
-      filter = function(client)
-        return client.name ~= "tsserver"
-      end
-    }
-  end
-
-  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
-  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-  -- vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-  vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
-  vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "show references" })
-  vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "previous diagnostic" })
-  vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "next diagnostic" })
-
-
-  if client.server_capabilities.documentFormattingProvider then
-    vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
-      callback = function()
-        format()
-      end,
-    })
-  end
-
-  vim.cmd [[autocmd CursorHold <buffer> lua vim.diagnostic.open_float({focus=false, border = "rounded"})]]
-  vim.cmd [[autocmd CursorHoldI <buffer> lua vim.diagnostic.open_float({focus=false, border = "rounded" })]]
-end
-
--- Go autoimport
-vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = { "*.go" },
-    callback = function()
-      local params = vim.lsp.util.make_range_params(nil, vim.lsp.util._get_offset_encoding())
-      params.context = {only = {"source.organizeImports"}}
-
-      local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
-      for _, res in pairs(result or {}) do
-        for _, r in pairs(res.result or {}) do
-          if r.edit then
-            vim.lsp.util.apply_workspace_edit(r.edit, vim.lsp.util._get_offset_encoding())
-          else
-            vim.lsp.buf.execute_command(r.command)
-          end
-        end
-      end
-    end,
-  })
-
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
   vim.lsp.diagnostic.on_publish_diagnostics, {
     underline = true,
@@ -95,7 +30,8 @@ require("null-ls").setup({
     require("null-ls").builtins.diagnostics.shellcheck,
     require("null-ls").builtins.diagnostics.erb_lint,
     require("null-ls").builtins.diagnostics.rubocop,
-    require("null-ls").builtins.formatting.vale,
+    -- require("null-ls").builtins.formatting.vale,
+    require("null-ls").builtins.formatting.prettier,
   },
 })
 
@@ -158,12 +94,82 @@ cmp.setup.cmdline(':', {
       })
   })
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-require'lspconfig'.tsserver.setup { on_attach = on_attach, capabilities = capabilities }
+local on_attach = function(client, bufnr)
+  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+
+  local signs = { Error = "✕", Warn = "! ", Hint = "★ ", Info = "i " }
+  for type, icon in pairs(signs) do
+    local hl = "DiagnosticSign" .. type
+    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+  end
+
+  local format = function()
+    vim.lsp.buf.format {
+      async = false,
+      filter = function(client)
+        return client.name ~= "tsserver"
+      end
+    }
+  end
+
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+  -- vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+  vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
+  vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "show references" })
+  vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "previous diagnostic" })
+  vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "next diagnostic" })
+
+
+  if client.server_capabilities.documentFormattingProvider then
+    vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
+      buffer = bufnr,
+      callback = function()
+        format()
+      end,
+    })
+  end
+
+  vim.cmd [[autocmd CursorHold <buffer> lua vim.diagnostic.open_float({focus=false, border = "rounded"})]]
+  vim.cmd [[autocmd CursorHoldI <buffer> lua vim.diagnostic.open_float({focus=false, border = "rounded" })]]
+end
+
+-- Go autoimport
+vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = { "*.go" },
+    callback = function()
+      local params = vim.lsp.util.make_range_params(nil, vim.lsp.util._get_offset_encoding())
+      params.context = {only = {"source.organizeImports"}}
+
+      local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+      for _, res in pairs(result or {}) do
+        for _, r in pairs(res.result or {}) do
+          if r.edit then
+            vim.lsp.util.apply_workspace_edit(r.edit, vim.lsp.util._get_offset_encoding())
+          else
+            vim.lsp.buf.execute_command(r.command)
+          end
+        end
+      end
+    end,
+  })
+
+
+require'lspconfig'.tsserver.setup {
+  on_attach = on_attach,
+  capabilities = capabilities
+}
+require'lspconfig'.golangci_lint_ls.setup{ on_attach = on_attach, capabilities = capabilities }
 require'lspconfig'.gopls.setup{
   on_attach = on_attach,
   capabilities = capabilities,
 }
 require'lspconfig'.solargraph.setup{ on_attach = on_attach, capabilities = capabilities }
-
