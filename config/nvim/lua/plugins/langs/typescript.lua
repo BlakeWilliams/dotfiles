58b7e8e1d9d5e2eea9b235672cea1e5de87bc87d
Blake Williams <blake@blakewilliams.me>
2024-10-18 20:43:17 -0400

    Fix ts/js formatters

--- config/nvim/lua/plugins/langs/typescript.lua
+++ config/nvim/lua/plugins/langs/typescript.lua
@@ -88,8 +88,8 @@ return {
     "stevearc/conform.nvim",
     opts = {
       formatters_by_ft = {
-        javascript = { "prettierd", "prettier" },
-        typescript = { "prettierd", "prettier" },
+        javascript = { "prettierd", "prettier", stop_after_first = true },
+        typescript = { "prettierd", "prettier", stop_after_first = true },
       },
     },
   },
