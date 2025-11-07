ee095a74816346263128184b4a9e270825f28f8d
Blake Williams <blake@blakewilliams.me>
2025-11-06 19:51:22 -0500

    Remove unused files

--- config/nvim/lua/plugins/neotree.lua
+++ config/nvim/lua/plugins/neotree.lua
@@ -81,17 +81,6 @@ return {
           folder_closed = "",
           folder_open = "",
           folder_empty = "󰜌",
-          provider = function(icon, node, state) -- default icon provider utilizes nvim-web-devicons if available
-            if node.type == "file" or node.type == "terminal" then
-              local success, web_devicons = pcall(require, "nvim-web-devicons")
-              local name = node.type == "terminal" and "terminal" or node.name
-              if success then
-                local devicon, hl = web_devicons.get_icon(name)
-                icon.text = "" -- devicon or icon.text
-                icon.highlight = hl or icon.highlight
-              end
-            end
-          end,
         },
         indent = {
           with_expanders = false,
