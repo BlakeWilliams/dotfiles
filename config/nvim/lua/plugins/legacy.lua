dbe736ab44c1f7027f7c395531906bd684bda9ff
Blake Williams <blake@blakewilliams.me>
2025-12-25 16:38:28 -0500

    Setup goyo

--- config/nvim/lua/plugins/legacy.lua
+++ config/nvim/lua/plugins/legacy.lua
@@ -20,4 +20,32 @@ return {
   { 'folke/tokyonight.nvim' },
   { 'rose-pine/neovim' },
   { 'maxmx03/solarized.nvim' },
+  {
+    'junegunn/goyo.vim',
+    init = function()
+      vim.g.goyo_auto_leave = 0
+
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "GoyoEnter",
+        callback = function()
+          vim.o.laststatus = 0
+          vim.o.showtabline = 0
+          vim.o.signcolumn = 'no'
+          vim.opt_local.linebreak = true
+          require("lualine").hide()
+        end,
+      })
+
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "GoyoLeave",
+        callback = function()
+          vim.o.laststatus = 2
+          vim.o.showtabline = 2
+          vim.o.signcolumn = 'yes:1'
+          vim.opt_local.linebreak = false
+          require("lualine").hide({ unhide = true })
+        end,
+      })
+    end
+  },
 }
